83723bc0339b608f893e8f6b5011baaa897a0901
nguyenNgocHoang <batrna2006@gmail.com>
2026-04-22 11:20:31 +0700

    Update the Store class to work with Media

--- java/src/hust/soict/hedspi/aims/store/Store.java
+++ java/src/hust/soict/hedspi/aims/store/Store.java
@@ -1,20 +1,24 @@
 package hust.soict.hedspi.aims.store;
-
-import hust.soict.hedspi.aims.media.DigitalVideoDisc;
+import hust.soict.hedspi.aims.media.Media;
 
 import java.util.ArrayList;
 
 public class Store {
-    private ArrayList<DigitalVideoDisc> itemsInStore = new ArrayList<>();
-    public void addDVD(DigitalVideoDisc disc) {
-        itemsInStore.add(disc);
-        System.out.println("The disc "+ disc.getTitle() +" has been added");
+    private ArrayList<Media> itemsInStore = new ArrayList<Media>();
+    public void addMedia(Media media) {
+        if (itemsInStore.contains(media)) {
+            System.out.println("The media " + media.getTitle() + " is already in the store");
+        } else {
+            itemsInStore.add(media);
+            System.out.println("The media " + media.getTitle() + " has been added to the store");
+        }
     }
-    public void removeDVD(DigitalVideoDisc disc) {
-        itemsInStore.remove(disc);
-        System.out.println("The disc "+ disc.getTitle() +" has been removed");
+    public void removeMedia(Media media) {
+        if (itemsInStore.contains(media)) {
+            itemsInStore.remove(media);
+            System.out.println("The media " + media.getTitle() + " has been removed from the store");
+        } else {
+            System.out.println("The media " + media.getTitle() + " is not in the store");
+        }
     }
-
-
-
 }
